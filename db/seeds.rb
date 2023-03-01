36ea8e3980c8d498a71bf677fc43718f031d5f86
Ezequiel <epuyosdacosta@gmail.com>
2023-03-01 21:13:22 +0100

    Added seed for enrollments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ Enrollment.destroy_all
 
 puts "Creating..."
 
+
 # USERS
 
 admin_user = User.create(
@@ -55,31 +56,84 @@ puts "User with id: #{user.id} has been created"
 # COURSES
 
 course = Course.create(
+  id: 1,
   title: "Design 488",
   user: admin_user
 )
 puts "Course with id: #{course.id} has been created"
 
 course = Course.create(
+  id: 2,
   title: "Criminology 425",
   user: admin_user
 )
 puts "Course with id: #{course.id} has been created"
 
 course = Course.create(
+  id: 3,
   title: "Computer Science 235",
   user: admin_user
 )
 puts "Course with id: #{course.id} has been created"
 
 course = Course.create(
+  id: 4,
   title: "Creative Arts 297",
   user: admin_user
 )
 puts "Course with id: #{course.id} has been created"
 
 course = Course.create(
+  id: 5,
   title: "Applied Science (Psychology) 543",
   user: admin_user
 )
 puts "Course with id: #{course.id} has been created"
+
+
+# ENROLLMENTS
+
+enrollment = Enrollment.create(
+  course_id: 2,
+  user_id: 1,
+  pass_score: 50,
+  progress: 75,
+  score: 80
+)
+puts "Enrollment with id: #{enrollment.id} has been created"
+
+enrollment = Enrollment.create(
+  course_id: 1,
+  user_id: 1,
+  pass_score: 80,
+  progress: 100,
+  score: 100
+)
+puts "Enrollment with id: #{enrollment.id} has been created"
+
+enrollment = Enrollment.create(
+  course_id: 4,
+  user_id: 1,
+  pass_score: 50,
+  progress: 90,
+  score: 100
+)
+puts "Enrollment with id: #{enrollment.id} has been created"
+
+enrollment = Enrollment.create(
+  course_id: 5,
+  user_id: 1,
+  pass_score: 80,
+  progress: 75,
+  score: 80
+)
+puts "Enrollment with id: #{enrollment.id} has been created"
+
+enrollment = Enrollment.create(
+  course_id: 3,
+  user_id: 1,
+  pass_score: 50,
+  progress: 75,
+  score: 100
+)
+puts "Enrollment with id: #{enrollment.id} has been created"
