f60227c8929ba69501a41504d77832282f46ec79
Ezequiel <epuyosdacosta@gmail.com>
2023-03-01 16:58:05 +0100

    Added admin boolean and name to users

---
+++ db/migrate/20230301155606_add_admin_to_users.rb
@@ -0,0 +1,6 @@
+class AddAdminToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :admin, :boolean, null: false, default: false
+  end
+end
